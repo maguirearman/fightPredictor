31cc42ef4e09ee29ed4035c82137e38b137d528c
maguirearman <arman.29@osu.edu>
2024-02-24 16:38:05 -0500

    tried implementing random forest model (horrible performance and some errors)

--- mma_proj/data_pp_cleanup.py
+++ mma_proj/data_pp_cleanup.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+
 
 def read_data():
     ufc_events = pd.read_csv('archive/ufc_event_data.csv')
@@ -69,17 +71,16 @@ def feature_selection(merged_data):
         print("'winner' column not found in selected features.")
         return None
 
-def train_logistic_regression(data):
+def train_random_forest(data):
     # Separate features and target variable
     X = data.drop(columns=['winner'])
     y = data['winner']
     
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
     
-    # Initialize and train the logistic regression model
-    model = LogisticRegression(max_iter=1000)  # Increase max_iter if needed
+    # Initialize and train the Random Forest model
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     
     # Predict on the test set
@@ -107,9 +108,9 @@ def main():
     
     # If selected_data is not None, save it to CSV
     if selected_data is not None:
-    #     model, accuracy, report = train_logistic_regression(selected_data)
-    #     print("Accuracy: ", accuracy)
-    #     print("Classification Report:\n", report)
+        model, accuracy, report = train_random_forest(selected_data)
+        print("Accuracy: ", accuracy)
+        # print("Classification Report:\n", report)
         selected_data.to_csv('data/selected_data.csv', index=False)
     else:
         print("No features selected. Nothing saved to CSV.")

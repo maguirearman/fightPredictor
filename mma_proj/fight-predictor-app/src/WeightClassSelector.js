a3d848f0f2d42435c7b1f77c6aa0a0975d6a100d
maguirearman <arman.29@osu.edu>
2024-04-03 18:56:39 -0400

    fixed weight class selector

--- mma_proj/fight-predictor-app/src/WeightClassSelector.js
+++ mma_proj/fight-predictor-app/src/WeightClassSelector.js
@@ -1,19 +1,34 @@
 // WeightClassSelector.js
-
 import React from 'react';
+import Select from 'react-select';
 
 function WeightClassSelector({ weightClasses, selectedWeightClass, onSelectWeightClass }) {
+  // Convert your weightClasses array for use with react-select
+  const options = weightClasses.map(weightClass => ({
+    value: weightClass,
+    label: weightClass // The text that will be displayed
+  }));
+
+  const handleChange = selectedOption => {
+    // Call onSelectWeightClass with the value of the selected option
+    // or an empty string if no option is selected (cleared selection)
+    onSelectWeightClass(selectedOption ? selectedOption.value : '');
+  };
+
+  // Find the currently selected option
+  const selectedOption = options.find(option => option.value === selectedWeightClass) || null;
+
   return (
-    <div>
-      <label htmlFor="weightClass">Select a Weight Class:</label>
-      <select id="weightClass" value={selectedWeightClass} onChange={(e) => onSelectWeightClass(e.target.value)}>
-        <option value="">Select</option>
-        {weightClasses.map((weightClass) => (
-          <option key={weightClass} value={weightClass}>
-            {weightClass}
-          </option>
-        ))}
-      </select>
+    <div className='weight-selector'>
+      <label htmlFor="weight-class-select">Select a Weight Class:</label>
+      <Select
+        inputId="weight-class-select" // Ensures the label is properly associated
+        value={selectedOption}
+        onChange={handleChange}
+        options={options}
+        isClearable={true} // Allows users to clear the selected value
+        placeholder="Select a weight class"
+      />
     </div>
   );
 }

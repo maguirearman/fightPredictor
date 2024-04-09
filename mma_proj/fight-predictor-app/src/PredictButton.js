84894aad39dbf4930575e9640302b55033654a65
maguirearman <arman.29@osu.edu>
2024-04-08 21:44:52 -0400

    worked on adding button

--- mma_proj/fight-predictor-app/src/PredictButton.js
+++ mma_proj/fight-predictor-app/src/PredictButton.js
@@ -1,11 +1,22 @@
 // PredictButton.js
-
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function PredictButton({ onClick }) {
   return (
     <div>
-      <button className="selector" onClick={onClick}>Predict Fight</button>
+      <Button
+        variant="contained"
+        onClick={onClick}
+        sx={{
+          backgroundColor: '#8B0000', // Dark red, similar to "blood red"
+          '&:hover': {
+            backgroundColor: '#640000', // A darker shade for hover state
+          },
+        }}
+      >
+        Predict Fight
+      </Button>
     </div>
   );
 }

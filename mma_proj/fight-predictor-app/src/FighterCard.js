013827c03371301574a81a45d7a0f2dc3fa18775
maguirearman <arman.29@osu.edu>
2024-04-24 17:03:46 -0400

    finished up fighter cards for now

--- mma_proj/fight-predictor-app/src/FighterCard.js
+++ mma_proj/fight-predictor-app/src/FighterCard.js
@@ -7,36 +7,36 @@ function FighterCard({ fighter }) {
   const aspectRatio = (700 / 460) * 100; // Calculate the height as a percentage of the width
 
 
-  return (
-    <Card sx={{ maxWidth: 250, m: 2 }}> 
-      <Box sx={{ width: '100%', pt: `${aspectRatio}%`, position: 'relative' }}> 
-        <CardMedia
-          component="img"
-          sx={{
-            position: 'absolute', // Absolutely position the image to cover the box
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover' // Cover ensures the image covers the box area, cropping if necessary
-          }}
-          image={fighter.image}
-          alt={fighter.name}
-        />
-      </Box>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {fighter.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Ranking: {fighter.ranking} {/* Example additional info */}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Weight Class: {fighter.weightClass} {/* More info */}
-        </Typography>
-      </CardContent>
+return (
+    <Card sx={{ maxWidth: 250, m: 2, padding: 0 }}> 
+        <Box sx={{ width: '100%', pt: `${aspectRatio}%`, position: 'relative' }}> 
+            <CardMedia
+                component="img"
+                sx={{
+                    position: 'absolute', // Absolutely position the image to cover the box
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover' // Cover ensures the image covers the box area, cropping if necessary
+                }}
+                image={fighter.image}
+                alt={fighter.name}
+            />
+        </Box>
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {fighter.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                Ranking: {fighter.ranking} {/* Example additional info */}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                Weight Class: {fighter.weightClass} {/* More info */}
+            </Typography>
+        </CardContent>
     </Card>
-  );
+);
 }
 
 export default FighterCard;

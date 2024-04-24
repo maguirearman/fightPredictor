b3d8bf5c94e9a831b91b34585f7db4ba1b56ff43
maguirearman <arman.29@osu.edu>
2024-04-24 17:48:50 -0400

    reformatted data for fighter cards

---
+++ mma_proj/fight-predictor-app/src/data/fighterCardData.js
@@ -0,0 +1,114 @@
+// src/data/fighterCardData.js
+
+const fighterCardData = [
+    {
+      id: 1,
+      name: "Islam Makhachev",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-10/MAKHACHEV_ISLAM_BELT_L_10-21.png?itok=glu3eHJa",
+      ranking: "#1 Pound for Pound, Lightweight Champion",
+      weightClass: "Lightweight"
+    },
+    {
+      id: 2,
+      name: "Jon Jones",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-03/JONES_JON_L_BELT_03_04.png?itok=P6J6DQpm",
+      ranking: "#1 Pound for Pound, Heavyweight Champion",
+      weightClass: "Heavyweight"
+    },
+    {
+      id: 3,
+      name: "Leon Edwards",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-12/EDWARDS_LEON_BELT_L_12-16.png?itok=RVcuzrpG",
+      ranking: "#3 Pound for Pound, Welterweight Champion",
+      weightClass: "Welterweight"
+    },
+    {
+      id: 4,
+      name: "Alex Pereira",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-04/PEREIRA_ALEX_L_BELT_04-13.png?itok=-zKSPFcu",
+      ranking: "#4 Pound for Pound, Light Heavyweight Champion, Former Middleweight Champion",
+      weightClass: "Light Heavyweight"
+    },
+    {
+      id: 5,
+      name: "Ilia Topuria",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-02/TOPURIA_ILIA_L_BELT_02-17.png?itok=h1fu9Vu9",
+      ranking: "#5 Pound for Pound, Featherweight Champion",
+      weightClass: "Featherweight"
+
+    }, 
+    {
+      id: 6,
+      name: "Sean O'Malley",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-03/OMALLEY_SEAN_BELT_L_03-09.png?itok=1XrvvVTW",
+      ranking: "#6 Pound for Pound, Bantamweight Champion",
+      weightClass: "Bantamweight"
+    },
+    {
+      id: 7,
+      name: "Alexander Volkanovski",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-02/VOLKANOVSKI_ALEXANDER_L_02-17.png?itok=Vu9a3K_h",
+      ranking: "#7 Pound for Pound, Featherweight Champion",
+      weightClass: "Featherweight"
+    },
+    {
+      id: 8,
+      name: "Max Holloway",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-04/HOLLOWAY_MAX_L_04-13.png?itok=U9IB8OUQ",
+      ranking: "#8 Pound for Pound, BMF Champion, #9 Lightweight, #2 Featherweight",
+      weightClass: "Featherweight, Lightweight"
+    },
+    {
+      id: 9,
+      name: "Dricus Du Plessis",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-01/DU_PLESSUS_DRICUS_L_BELTMOCK.png?itok=Te4zddjX",
+      ranking: "#9 Pound for Pound, Middleweight Champion",
+      weightClass: "Middleweight"
+    }, 
+    {
+      id: 10,
+      name: "Alexandre Pantoja",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-12/PANTOJA_ALEXANDRE_L_12-16.png?itok=KUFPMuog",
+      ranking: "#10 Pound for Pound, Flyweight Champion",
+      weightClass: "Flyweight"
+    },
+    {
+      id: 11,
+      name: "Charles Oliveira",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-04/OLIVEIRA_CHARLES_L_04-13.png?itok=zuTsqmoN",
+      ranking: "#11 Pound for Pound, #2 Lightweight",
+      weightClass: "Lightweight"
+    },
+    {
+      id: 12,
+      name: "Israel Adesanya",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-09/ADESANYA_ISRAEL_L_04-08.png?itok=FOMzxCwD",
+      ranking: "#12 Pound for Pound, #2 Middleweight",
+      weightClass: "Middleweight"
+    },
+    {
+      id: 13,
+      name: "Tom Aspinall",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-01/ASPINALL_TOM_L_BELTMOCK.png?itok=b-8iWaRT",
+      ranking: "#13 Pound for Pound, Interim Heavyweight Champion",
+      weightClass: "Heavyweight"
+    }, 
+    {
+      id: 14,
+      name: "Sean Strickland",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-06/STRICKLAND_SEAN_L_07-01.png?itok=Xa9H8rts",
+      ranking: "#14 Pound for Pound, #1 Middleweight",
+      weightClass: "Middleweight"
+    }, 
+    {
+      id: 15,
+      name: "Aljermain Sterling",
+      image: "https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2024-04/STERLING_ALJAMAIN_L_04-13.png?itok=T1vPSqkE",
+      ranking: "#15 Pound for Pound, #8 Featherweight",
+      weightClass: "Bantamweight"
+    }
+
+    // Add more fighters as needed
+  ];
+
+export default fighterCardData;
